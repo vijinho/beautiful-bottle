b0f12d3b96103ce5b1454d879d7ed4ef2db8df99
Vijay Mahrra <vijay.mahrra@gmail.com>
2015-04-01 14:28:33 +0100

    people will complain if there's no comments

--- webapp.py
+++ webapp.py
@@ -5,14 +5,17 @@ from bottle import error, route, static_file, default_app, run
 
 @error(404)
 def error404(error):
+    """Display the error 404 page"""
     return server_static('error/404.html')
 
 @route('/<filepath:path>')
 def server_static(filepath):
+    """Display static files in the web root folder"""
     return static_file(filepath, root='www')
 
 @route('/')
 def index():
+    """Display the homepage"""
     return static_file('index.html', root='www')
 
 application=default_app()
